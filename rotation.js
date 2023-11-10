92a8da7baa6af3021da015095d646a8da949b3da
Patrick James <patrick.james@aalto.fi>
2023-11-10 19:37:45 +0200

    Update login form validation to prevent SQL injection.

--- rotation.js
+++ rotation.js
@@ -1,18 +1,33 @@
-if (window.DeviceOrientationEvent) {
-    window.addEventListener(
-      "deviceorientation",
-      (event) => {
-        const rotateDegrees = event.alpha; // alpha: rotation around z-axis
-        const leftToRight = event.gamma; // gamma: left to right
-        const frontToBack = event.beta; // beta: front back motion
-  
-        handleOrientationEvent(frontToBack, leftToRight, rotateDegrees);
-      },
-      true,
-    );
+// Define Rotation class
+class Rotation {
+  constructor() {
+    this.rotationValue = 0;
+    if (window.DeviceOrientationEvent) {
+      window.addEventListener(
+        "deviceorientation",
+        (event) => {
+          const rotateDegrees = event.alpha; // alpha: rotation around z-axis
+          const leftToRight = event.gamma; // gamma: left to right
+          const frontToBack = event.beta; // beta: front back motion
+
+          this.rotationValue = frontToBack; // Use frontToBack as rotation value
+
+          handleOrientationEvent(frontToBack, leftToRight, rotateDegrees);
+        },
+        true
+      );
+    }
   }
-  
-  const handleOrientationEvent = (frontToBack, leftToRight, rotateDegrees) => {
-    // do something amazing
-  };
-  
+
+  getRotation() {
+    return this.rotationValue;
+  }
+}
+
+const handleOrientationEvent = (frontToBack, leftToRight, rotateDegrees) => {
+  // do something amazing
+  // You can add your custom logic here
+};
+
+// Export the Rotation class
+window.Rotation = Rotation;
