beb91ae18c8cf42ab6935d6b12faa44fabede4d8
Patrick James <patrick.james@aalto.fi>
2023-11-10 18:31:20 +0200

    first commit

---
+++ rotation.js
@@ -0,0 +1,18 @@
+if (window.DeviceOrientationEvent) {
+    window.addEventListener(
+      "deviceorientation",
+      (event) => {
+        const rotateDegrees = event.alpha; // alpha: rotation around z-axis
+        const leftToRight = event.gamma; // gamma: left to right
+        const frontToBack = event.beta; // beta: front back motion
+  
+        handleOrientationEvent(frontToBack, leftToRight, rotateDegrees);
+      },
+      true,
+    );
+  }
+  
+  const handleOrientationEvent = (frontToBack, leftToRight, rotateDegrees) => {
+    // do something amazing
+  };
+  
