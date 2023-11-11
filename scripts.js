aed02754a283ed2779bb26e798f446406e09bd96
PatrickJamesDev <patrick.james@aalto.fi>
2023-11-11 11:48:39 +0200

    Separating js and html

---
+++ scripts.js
@@ -0,0 +1,31 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const rotation = new Rotation();
+
+    webgazer.setRegression('ridge')
+        .setTracker('clmtrackr')
+        .setGazeListener(function(data, clock) {
+            if (data == null) {
+                return;
+            }
+
+            const faceModel = data.face; // Access face model data
+            if (faceModel) {
+                const faceAngle = faceModel.angle; // Get the angle of the face
+                document.getElementById("face-angle").value = faceAngle.toFixed(2);
+            }
+
+            const phoneAngle = rotation.getRotation(); // Get the angle of the phone
+            document.getElementById("rotation").value = phoneAngle.toFixed(2);
+        })
+        .begin();
+    webgazer.showPredictionPoints(true);
+
+    setInterval(() => {
+        const phoneRotation = rotation.getRotation();
+        if (phoneRotation > 10) {
+            document.getElementById("exercise").style.display = "block";
+        } else {
+            document.getElementById("exercise").style.display = "none";
+        }
+    }, 10); // Update every 0.01 second, adjust as needed
+});
